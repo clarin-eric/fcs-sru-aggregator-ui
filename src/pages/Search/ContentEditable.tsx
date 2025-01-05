9905df14091e710bc7669d2024136dc78648b254
Erik Körner <koerner@saw-leipzig.de>
2025-01-05 16:28:37 +0100

    ContentEditable Finish, WIP Syntax highlighting

--- src/pages/Search/ContentEditable.tsx
+++ src/pages/Search/ContentEditable.tsx
@@ -1,4 +1,4 @@
-import React, { useImperativeHandle, useRef } from 'react'
+import React, { useEffect, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react'
 
 // --------------------------------------------------------------------------
 // types
@@ -172,157 +172,34 @@ function getCursorPosition(container: Node) {
 
 const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
   ({ value, onChange, disabled, placeholder, ...props }, ref) => {
+    // ref to hidden input element (for form stuffs)
     const inputRef = useRef<HTMLInputElement>(null)
+    // ref to our contentEditable input
     const mRef = useRef<HTMLDivElement>(null)
     // allow forwardRefs but use our local one since we need access
     // see: https://stackoverflow.com/a/76739143/9360161
     useImperativeHandle(ref, () => mRef.current as HTMLDivElement)
 
-    const sanitizedValue = sanitizeValue(value)
-    const defaultValue = useRef(sanitizedValue)
+    const [sanitizedValue, setSanitizedValue] = useState(sanitizeValue(value))
+    const [cursorPos, setCursorPos] = useState<number | null>(null)
 
-    if (mRef && mRef.current) {
-      const mValue = mRef.current.innerText
-      console.debug('[root]', { mValue, value, sanitizedValue })
-      if (mValue !== value) {
-        // update our value when change from outside
-        defaultValue.current = sanitizedValue
-      } else {
-        // update on change in "textfield" (inside)
+    useEffect(() => {
+      // if (disabled) return
 
-        updateInnerHTMLWithCursors(fancifyValue(sanitizedValue))
+      setSanitizedValue(sanitizeValue(value))
+
+      if (mRef.current) {
+        const pos = getCursorPosition(mRef.current)
+        setCursorPos(pos)
       }
+    }, [value])
 
-      // TODO: update when inside, so textual content is same but we want to add highlighting
-      // need to keep cursor
-    }
-
-    // ------------------------------------------------------------------------
-
-    function updateInnerHTMLWithCursors(value: string) {
-      if (!mRef.current) return
-
-      const container = mRef.current
-      const isTargetFocused = document.activeElement === container
-
-      let selectionIndices = null
-      const selection = window.getSelection()
-
-      // query initial cursor position (if possible)
-      if (isTargetFocused && selection && selection.rangeCount > 0) {
-        const range = selection.getRangeAt(0)
-        selectionIndices = getAbsoluteTextCursorOffsets(container, range)
-
-        console.log('selection before', range, selectionIndices, {
-          container: container.textContent,
-          value,
-          sanitizedValue,
-          defaultValue: defaultValue.current,
-        })
+    useLayoutEffect(() => {
+      if (mRef.current && cursorPos !== null) {
+        // only if we have a valid cursor, otherwise let default behaviour do the work
+        setCursorPosition(mRef.current, cursorPos)
       }
-
-      // update element content
-      mRef.current.innerHTML = value
-
-      // try to restore cursor position
-      if (isTargetFocused && selection) {
-        const range = document.createRange()
-
-        if (selectionIndices) {
-          console.log('[updateInnerHTMLWithCursors] set range', selectionIndices[0])
-
-          const startRange = getRangeContainerWithOffsetFromTextOffset(
-            container,
-            selectionIndices[0]
-          )
-          if (startRange) {
-            range.setStart(startRange.node, startRange.offset)
-
-            const endRange = getRangeContainerWithOffsetFromTextOffset(
-              container,
-              selectionIndices[1]
-            )
-            if (endRange) {
-              range.setEnd(endRange.node, endRange.offset)
-            } else {
-              range.collapse(true)
-            }
-          }
-        } else {
-          // TODO: unsure?
-          console.warn('something unsure happened')
-          range.setStart(container, container.textContent?.length ?? 0)
-          range.collapse(true)
-        }
-
-        selection.removeAllRanges()
-        selection.addRange(range)
-      }
-    }
-
-    function doRemove(lastRange: boolean) {
-      if (!mRef.current) return null
-
-      const container = mRef.current
-      const isTargetFocused = document.activeElement === container
-      if (!isTargetFocused) return null
-
-      const selection = window.getSelection()
-      if (!selection) return null
-      if (selection.rangeCount === 0) return null
-
-      const selectionIndices: Array<number[]> = []
-      for (let i = 0; i < selection.rangeCount; i++) {
-        const range = selection.getRangeAt(i)
-        const idxRange = getAbsoluteTextCursorOffsets(container, range)
-        if (idxRange && idxRange[0] !== 0 && idxRange[1] !== 0) selectionIndices.push(idxRange)
-      }
-
-      // nothing to do
-      if (selectionIndices.length === 0) return null
-
-      if (lastRange) {
-        // sort, so we can delete correctly (from the back to keep indices valid)
-        selectionIndices.sort().reverse()
-
-        // how many chars deleted
-        const numDeleted = selectionIndices
-          .map(([start, end]) => (start < end ? end - start : 1))
-          .reduce((acc, cur) => acc + cur, 0)
-        // from end of last selection minus deleted chars will be new pos
-        // collapse multiple ranges into one for simplicity
-        const newCursorPos = selectionIndices[0][1] - numDeleted
-
-        const oldValue = container.innerText
-        const newValue = selectionIndices.reduce(
-          (text, [start, end]) =>
-            start < end
-              ? text.slice(0, start) + text.slice(end)
-              : text.slice(0, start - 1) + text.slice(start),
-          oldValue
-        )
-
-        return { value: newValue, offset: newCursorPos }
-      } else {
-        // sort, so we can delete correctly (from the back to keep indices valid)
-        selectionIndices.sort()
-
-        // find first selection
-        const newCursorPos = selectionIndices[0][1]
-
-        const oldValue = container.innerText
-        const newValue = selectionIndices
-          .toReversed()
-          .reduce((text, [start, end]) => text.slice(0, start) + text.slice(end), oldValue)
-
-        return { value: newValue, offset: newCursorPos }
-      }
-    }
-
-    function setCursor(offset: number) {
-      if (!mRef.current) return null
-      setCursorPosition(mRef.current, offset)
-    }
+    })
 
     // ------------------------------------------------------------------------
 
@@ -338,7 +215,7 @@ const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
 
     function handleInput(event: React.ChangeEvent<HTMLDivElement>) {
       const value = event.target.innerText
-      console.debug('[handleInput]', value)
+      // console.debug('[handleInput]', value)
 
       if (inputRef.current) {
         inputRef.current.value = value
@@ -348,18 +225,15 @@ const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
     }
 
     function handlePaste(event: React.ClipboardEvent<HTMLDivElement>) {
-      console.debug('[handlePaste]', event)
+      // console.debug('[handlePaste]', event)
 
-      // event.stopPropagation()
-      // event.preventDefault()
+      event.stopPropagation()
+      event.preventDefault()
 
       // see: https://stackoverflow.com/a/6804718/9360161
       // Get pasted data via clipboard API
       const clipboardData = event.clipboardData || window.clipboardData
       const pastedData = clipboardData.getData('text')
-
-      // sanitize input
-      // const sanitizedData = sanitizeValue(pastedData)
 
       handleInsert(pastedData)
     }
@@ -367,8 +241,12 @@ const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
     function handleKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
       // console.debug('[handleKeyDown]', event)
 
+      // TODO: handle Ctrl-Z?
+      // NOTE: Ctrl-X / cut is handled
+      // NOTE: (Shift-)Tab is handled
+
       if (event.key === 'Enter') {
-        console.debug('[handleKeyDown:Enter]')
+        // console.debug('[handleKeyDown:Enter]')
 
         event.stopPropagation()
         event.preventDefault()
@@ -381,57 +259,214 @@ const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
           inputRef.current.form?.requestSubmit()
         }
       } else if (event.key === 'Backspace') {
-        console.debug('[handleKeyDown#Backspace]')
+        // console.debug('[handleKeyDown#Backspace]')
 
         event.stopPropagation()
         event.preventDefault()
 
         // custom backspace deletion handling since backspace on formatted text
         // will jump the cursor to the left more than expected???
+        handleBackspace()
+      } else if (event.key === 'Delete') {
+        // console.debug('[handleKeyDown#Delete]')
+
+        event.stopPropagation()
+        event.preventDefault()
+
         handleDelete()
+      } else {
+        // default, do not attempt to set cursor
+        setCursorPos(null)
+      }
+    }
+
+    // ------------------------------------------------------------------------
+
+    function doRemove(mode: 'delete' | 'backspace' | 'paste') {
+      if (!mRef.current) return null
+
+      const container = mRef.current
+      const isTargetFocused = document.activeElement === container
+      if (!isTargetFocused) return null
+
+      const selection = window.getSelection()
+      if (!selection) return null
+      if (selection.rangeCount === 0) return null
+
+      const selectionIndices: Array<number[]> = []
+      for (let i = 0; i < selection.rangeCount; i++) {
+        const range = selection.getRangeAt(i)
+        const idxRange = getAbsoluteTextCursorOffsets(container, range)
+        if (idxRange) selectionIndices.push(idxRange)
+      }
+
+      // nothing to do
+      if (selectionIndices.length === 0) return null
+
+      // sort, so we can delete correctly (from the back to keep indices valid)
+      selectionIndices.sort()
+
+      // how many chars will deleted
+      const numDeleted = selectionIndices
+        .map(([start, end]) => end - start)
+        .reduce((acc, cur) => acc + cur, 0)
+
+      const oldValue = container.innerText // TODO: use container.textContent?
+      const newValueSpansRemoved = selectionIndices
+        .toReversed()
+        .reduce((text, [start, end]) => text.slice(0, start) + text.slice(end), oldValue)
+
+      if (mode === 'backspace') {
+        if (numDeleted === 0) {
+          // then perform normal backspace operation
+          const lastPos = selectionIndices
+            .filter(([start, end]) => start === end)
+            .map(([, end]) => end)
+            .pop()
+
+          if (lastPos !== undefined) {
+            // should only be a single cursor! (multiple cursors are not supported by default in browsers?)
+            const newValue =
+              newValueSpansRemoved.slice(0, lastPos - 1) + newValueSpansRemoved.slice(lastPos)
+
+            return { value: newValue, offset: lastPos - 1 }
+          }
+        } else {
+          // from end of last selection minus deleted chars will be new pos
+          // collapse multiple ranges into one for simplicity
+          const lastPos = selectionIndices.map(([, end]) => end).pop()
+
+          if (lastPos !== undefined) {
+            return { value: newValueSpansRemoved, offset: lastPos - numDeleted }
+          }
+        }
+
+        console.warn('invalid "backspace" process, abort', {
+          container,
+          selectionIndices,
+          oldValue,
+          newValueSpansRemoved,
+          numDeleted,
+        })
+        return null
+      } else if (mode === 'delete') {
+        if (numDeleted === 0) {
+          // then perform normal backspace operation
+          const lastPos = selectionIndices
+            .filter(([start, end]) => start === end)
+            .map(([, end]) => end)
+            .pop()
+
+          if (lastPos !== undefined) {
+            // should only be a single cursor! (multiple cursors are not supported by default in browsers?)
+            const newValue =
+              newValueSpansRemoved.slice(0, lastPos) + newValueSpansRemoved.slice(lastPos + 1)
+
+            return { value: newValue, offset: lastPos }
+          }
+        } else {
+          // from end of last selection minus deleted chars will be new pos
+          // collapse multiple ranges into one for simplicity
+          const lastPos = selectionIndices.map(([, end]) => end).pop()
+
+          if (lastPos !== undefined) {
+            return { value: newValueSpansRemoved, offset: lastPos - numDeleted }
+          }
+        }
+
+        console.warn('invalid "delete" process, abort', {
+          container,
+          selectionIndices,
+          oldValue,
+          newValueSpansRemoved,
+          numDeleted,
+        })
+        return null
+      } else if (mode === 'paste') {
+        if (numDeleted === 0) {
+          // just find first position
+          const firstPos = selectionIndices
+            .filter(([start, end]) => start === end)
+            .map(([start]) => start)
+            .shift()
+
+          if (firstPos !== undefined) {
+            return { value: newValueSpansRemoved, offset: firstPos }
+          }
+        } else {
+          const firstPos = selectionIndices.map(([start]) => start).shift()
+
+          if (firstPos !== undefined) {
+            return { value: newValueSpansRemoved, offset: firstPos }
+          }
+        }
+
+        console.warn('invalid "paste" process, abort', {
+          container,
+          selectionIndices,
+          oldValue,
+          newValueSpansRemoved,
+          numDeleted,
+        })
+        return null
       }
     }
 
     function handleInsert(value: string) {
-      const result = doRemove(false)
+      const result = doRemove('paste')
       if (!result) return
 
+      const valueToInsertSanitized = sanitizeValue(value)
+
       const { value: newValue, offset: insertPos } = result
-      const newValueWithInsert = newValue.slice(0, insertPos) + value + newValue.slice(insertPos)
+      const newValueWithInsert =
+        newValue.slice(0, insertPos) + valueToInsertSanitized + newValue.slice(insertPos)
       const newCursorPos = insertPos + value.length
 
-      // update value
-      const sanitizedValue = sanitizeValue(newValueWithInsert)
-      defaultValue.current = sanitizedValue
-      if (inputRef.current) {
-        inputRef.current.value = sanitizedValue
-      }
-      if (mRef.current) {
-        mRef.current.innerHTML = fancifyValue(sanitizedValue)
-      }
-      onChange?.(sanitizedValue)
+      setSanitizedValue(newValueWithInsert)
+      setCursorPos(newCursorPos)
 
-      // set cursor
-      setCursor(newCursorPos)
+      if (inputRef.current) {
+        inputRef.current.value = newValueWithInsert
+      }
+      onChange?.(newValueWithInsert)
     }
 
-    function handleDelete() {
-      const result = doRemove(true)
+    function handleBackspace() {
+      const result = doRemove('backspace')
       if (!result) return
 
       const { value: newValue, offset: newCursorPos } = result
 
       const sanitizedValue = sanitizeValue(newValue)
-      defaultValue.current = sanitizedValue
+
+      setSanitizedValue(sanitizedValue)
+      setCursorPos(newCursorPos)
+
       if (inputRef.current) {
         inputRef.current.value = sanitizedValue
       }
-      if (mRef.current) {
-        mRef.current.innerHTML = fancifyValue(sanitizedValue)
+      // if (mRef.current) {
+      //   mRef.current.innerHTML = fancifyValue(sanitizedValue)
+      // }
+      onChange?.(sanitizedValue)
+    }
+
+    function handleDelete() {
+      const result = doRemove('delete')
+      if (!result) return
+
+      const { value: newValue, offset: newCursorPos } = result
+
+      const sanitizedValue = sanitizeValue(newValue)
+
+      setSanitizedValue(sanitizedValue)
+      setCursorPos(newCursorPos)
+
+      if (inputRef.current) {
+        inputRef.current.value = sanitizedValue
       }
       onChange?.(sanitizedValue)
-
-      setCursor(newCursorPos)
     }
 
     // ------------------------------------------------------------------------
@@ -448,8 +483,9 @@ const ContentEditable = React.forwardRef<HTMLDivElement, Props>(
           onInput={handleInput}
           onPaste={handlePaste}
           onKeyDown={handleKeyDown}
+          tabIndex={disabled ? -1 : props.tabIndex}
           contentEditable // plaintext-only?
-          dangerouslySetInnerHTML={{ __html: fancifyValue(defaultValue.current) }}
+          dangerouslySetInnerHTML={{ __html: fancifyValue(sanitizedValue) }}
           ref={mRef}
         />
         {placeholder && value === '' && (
