4e7cf72ef0f0b0c7f47c04f47ac0742c73abfc15
Erik Körner <koerner@saw-leipzig.de>
2025-02-20 16:15:38 +0100

    Add single chunk vs multiple chunks build modes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import react from '@vitejs/plugin-react'
+import {
+  type InputOption,
+  type LogHandlerWithDefault,
+  type OutputOptions,
+  type RollupOptions,
+} from 'rollup'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { defineConfig, loadEnv, type UserConfig } from 'vite'
+import {
+  type BuildOptions,
+  defineConfig,
+  type ESBuildOptions,
+  loadEnv,
+  type UserConfig,
+} from 'vite'
 import version from 'vite-plugin-package-version'
 
-// TODO: 'node:' prefix?
 import { fileURLToPath, URL } from 'node:url'
 // import path from 'node:path'
 
@@ -16,6 +27,10 @@ const name = `${pkg.name}-${pkg.version}`
 const outputsLibPath = 'lib/'
 const outputsLibVenderPath = `${outputsLibPath}vender/`
 const outputsLibAssetsPath = `${outputsLibPath}assets/`
+// NOTE: that single chunk (bundle mode) build will not use the "lib/" prefix
+
+// flags
+const debug = false
 
 function resolve(url: string | URL) {
   // path.resolve(__dirname, url)
@@ -26,7 +41,10 @@ function resolve(url: string | URL) {
 export default defineConfig(({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
 
-  return {
+  const singleChunk = mode === "bundle"
+
+  // base configuration
+  const baseConfig = {
     // https://vite.dev/guide/build.html#relative-base
     base: './',
     build: {
@@ -35,88 +53,17 @@ export default defineConfig(({ mode }) => {
         input: [
           // main entry point
           resolve('./index.html'),
-          // separate output chunks (primarily for CSS)
-          'bootstrap/dist/css/bootstrap.min.css',
-          // separate out prismjs chunk
-          resolve('./src/vendor/prismjs'),
-          // resolve('./src/vendor/antlr4ng'),
         ],
         output: {
           assetFileNames(chunkInfo) {
-            if (chunkInfo.names.length === 1) {
-              // DEBUG
-              // console.debug('assetFileNames', {
-              //   name: chunkInfo.names,
-              //   originalFileNames: chunkInfo.originalFileNames,
-              //   sourceLen: chunkInfo.source.length,
-              // })
-              // main output chunks
-              if (chunkInfo.names.includes('index.css')) {
-                // lazy import modules
-                if (chunkInfo.originalFileNames.includes('src/components/QueryBuilder/index.ts')) {
-                  return `${outputsLibPath}${pkg.name}-query-builder-${pkg.version}.[ext]`
-                }
-                // main
-                return `${outputsLibPath}${name}.[ext]`
-              }
-              // known output chunks (vendor)
-              if (['bootstrap.css'].some((name) => chunkInfo.names.includes(name))) {
-                // TODO: or check if suffix matching to `originalFileNames`
-                return `${outputsLibVenderPath}[name].[ext]`
-              }
+            if (chunkInfo.names.includes('index.css')) {
+              return `${name}.[ext]`
             }
-            // default output chunks (assets)
-            return `${outputsLibAssetsPath}[name].[ext]`
+            return `assets/[name].[ext]`
           },
-          entryFileNames(chunkInfo) {
-            // DEBUG
-            // console.debug('entryFileNames', chunkInfo)
-            if (
-              chunkInfo.name === 'index' &&
-              chunkInfo.facadeModuleId?.endsWith('vendor/prismjs/index.ts')
-            ) {
-              return `${outputsLibVenderPath}prism.js`
-            }
-            // if (
-            //   chunkInfo.name === 'index' &&
-            //   chunkInfo.facadeModuleId?.endsWith('vendor/antlr4ng/index.ts')
-            // ) {
-            //   return `${outputsLibVenderPath}antlr.js`
-            // }
-            return `${outputsLibPath}${name}.js`
-          },
-          chunkFileNames(chunkInfo) {
-            // DEBUG
-            // console.debug('chunkFileNames', {
-            //   name: chunkInfo.name,
-            //   facadeModuleId: chunkInfo.facadeModuleId,
-            //   isDynamicEntry: chunkInfo.isDynamicEntry,
-            // })
-            if (chunkInfo.isDynamicEntry) {
-              if (chunkInfo.facadeModuleId?.endsWith('src/components/QueryBuilder/index.ts')) {
-                return `${outputsLibPath}${pkg.name}-query-builder-${pkg.version}.js`
-              }
-            }
-            return `[name].js`
-          },
-          // https://rollupjs.org/configuration-options/#output-manualchunks
-          manualChunks: {
-            // vendor
-            [`${outputsLibVenderPath}react`]: ['react', 'react-dom', 'react/jsx-runtime'],
-            [`${outputsLibVenderPath}react-ext`]: [
-              'react-router',
-              '@tanstack/react-query',
-              'axios',
-              'zustand',
-              '@nozbe/microfuzz/react',
-              'react-helmet-async',
-            ],
-            [`${outputsLibVenderPath}antlr`]: ['antlr4ng'],
-            // ui
-            [`${outputsLibVenderPath}bootstrap`]: ['react-bootstrap'],
-          },
+          entryFileNames: `${name}.js`,
+          chunkFileNames: `[name].js`,
         },
-        // treeshake?
       },
       // manifest: true,
       sourcemap: true,
@@ -161,6 +108,8 @@ export default defineConfig(({ mode }) => {
       'import.meta.env.DEPLOY_PATH': process.env.VITE_DEPLOY_PATH
         ? `"${process.env.VITE_DEPLOY_PATH}"`
         : '"/"',
+      // canonical URL for FCS SRU Aggregator
+      'import.meta.env.CANONCIAL_URL': '"https://contentsearch.clarin.eu"',
       // API base URL for FCS SRU Aggregator
       'import.meta.env.API_URL': process.env.VITE_API_URL
         ? `"${process.env.VITE_API_URL}"`
@@ -189,4 +138,116 @@ export default defineConfig(({ mode }) => {
       // extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.yaml'],
     },
   } satisfies UserConfig
+
+  if (singleChunk) {
+    // keep a single chunk
+    Object.assign(baseConfig.define, {
+      'import.meta.env.FEATURE_LAZY_LOADING': 'false',
+    } satisfies Record<string, unknown>)
+  } else {
+    // split into multiple chunks if not disabled
+    Object.assign(baseConfig.build.rollupOptions, {
+      input: [
+        ...baseConfig.build.rollupOptions.input,
+        // separate output chunks (primarily for CSS)
+        'bootstrap/dist/css/bootstrap.min.css',
+        // separate out prismjs chunk
+        resolve('./src/vendor/prismjs'),
+      ] satisfies InputOption,
+      output: {
+        assetFileNames(chunkInfo) {
+          if (chunkInfo.names.length === 1) {
+            // DEBUG
+            // console.debug('assetFileNames', {
+            //   name: chunkInfo.names,
+            //   originalFileNames: chunkInfo.originalFileNames,
+            //   sourceLen: chunkInfo.source.length,
+            // })
+            // main output chunks
+            if (chunkInfo.names.includes('index.css')) {
+              // lazy import modules
+              if (chunkInfo.originalFileNames.includes('src/components/QueryBuilder/index.ts')) {
+                return `${outputsLibPath}${pkg.name}-query-builder-${pkg.version}.[ext]`
+              }
+              // main
+              return `${outputsLibPath}${name}.[ext]`
+            }
+            // known output chunks (vendor)
+            if (['bootstrap.css'].some((name) => chunkInfo.names.includes(name))) {
+              // TODO: or check if suffix matching to `originalFileNames`
+              return `${outputsLibVenderPath}[name].[ext]`
+            }
+          }
+          // default output chunks (assets)
+          return `${outputsLibAssetsPath}[name].[ext]`
+        },
+        entryFileNames(chunkInfo) {
+          // DEBUG
+          // console.debug('entryFileNames', chunkInfo)
+          if (
+            chunkInfo.name === 'index' &&
+            chunkInfo.facadeModuleId?.endsWith('vendor/prismjs/index.ts')
+          ) {
+            return `${outputsLibVenderPath}prism.js`
+          }
+          // if (
+          //   chunkInfo.name === 'index' &&
+          //   chunkInfo.facadeModuleId?.endsWith('vendor/antlr4ng/index.ts')
+          // ) {
+          //   return `${outputsLibVenderPath}antlr.js`
+          // }
+          return `${outputsLibPath}${name}.js`
+        },
+        chunkFileNames(chunkInfo) {
+          // DEBUG
+          // console.debug('chunkFileNames', {
+          //   name: chunkInfo.name,
+          //   facadeModuleId: chunkInfo.facadeModuleId,
+          //   isDynamicEntry: chunkInfo.isDynamicEntry,
+          // })
+          if (chunkInfo.isDynamicEntry) {
+            if (chunkInfo.facadeModuleId?.endsWith('src/components/QueryBuilder/index.ts')) {
+              return `${outputsLibPath}${pkg.name}-query-builder-${pkg.version}.js`
+            }
+          }
+          return `[name].js`
+        },
+        // https://rollupjs.org/configuration-options/#output-manualchunks
+        manualChunks: {
+          // vendor
+          [`${outputsLibVenderPath}react`]: ['react', 'react-dom', 'react/jsx-runtime'],
+          [`${outputsLibVenderPath}react-ext`]: [
+            'react-router',
+            '@tanstack/react-query',
+            'axios',
+            'zustand',
+            '@nozbe/microfuzz/react',
+            'react-helmet-async',
+          ],
+          // ui
+          [`${outputsLibVenderPath}bootstrap`]: ['react-bootstrap'],
+        },
+      } satisfies OutputOptions,
+    })
+  }
+
+  // debug options to print more logging messages (development stuff)
+  if (debug) {
+    Object.assign(baseConfig.esbuild, {
+      logLevel: 'verbose',
+    } satisfies ESBuildOptions)
+    Object.assign(baseConfig.build.rollupOptions, {
+      logLevel: 'debug',
+      onLog: ((level, log, defaultHandler) => {
+        console.log(`[${level}]: ${log}`)
+        defaultHandler?.(level, log)
+      }) satisfies LogHandlerWithDefault,
+    } satisfies RollupOptions)
+    Object.assign(baseConfig.build, {
+      manifest: true,
+      minify: false,
+    } satisfies BuildOptions)
+  }
+
+  return baseConfig
 })
